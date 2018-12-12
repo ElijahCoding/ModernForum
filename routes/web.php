65fefefb6c0838af30935a768fa5fd9e99298ed9
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-12 17:47:56 +0300

    uploading avatar test

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,4 @@ Route::get('/profiles/{user}/notifications', 'UserNotificationsController@index'
 Route::delete('/profiles/{user}/notifications/{notification}', 'UserNotificationsController@destroy');
 
 Route::get('api/users', 'Api\UserController@index');
+Route::post('api/users/{user}/avatar', 'Api\UserAvatarController@store');
--- tests/Feature/AddAvatarTest.php
+++ tests/Feature/AddAvatarTest.php
@@ -2,20 +2,46 @@
 
 namespace Tests\Feature;
 
+use Storage;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AddAvatarTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    /** @test */
+    function only_members_can_add_avatars()
     {
-        $this->assertTrue(true);
+        $this->withExceptionHandling();
+
+        $this->json('POST', 'api/users/1/avatar')
+             ->assertStatus(401);
+    }
+
+    /** @test */
+    function a_valid_avatar_must_be_provided()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $this->json('POST', "api/users/" . auth()->id() . '/avatar', [
+            'avatar' => 'not-an-image'
+        ])->assertStatus(422);
+    }
+
+    /** @test */
+    function a_user_may_add_an_avatar_to_their_profile()
+    {
+        $this->signIn();
+
+        Storage::fake('public');
+
+        $this->json('POST', "api/users/" . auth()->id() . '/avatar', [
+            'avatar' => $file = UploadedFile::fake()->image('avatar.jpg')
+        ]);
+
+        $this->assertEquals(asset('avatars/'.$file->hashName()), env('APP_URL') . '/' . auth()->user()->avatar_path);
+        Storage::disk('public')->assertExists('avatars/' . $file->hashName());
     }
 }
