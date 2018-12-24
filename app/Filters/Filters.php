e537d16a3888d9ee58a77cda661c998ac717ae94
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-24 13:21:47 +0300

    upgraded laravel 5.4 to 5.5

--- app/Filters/Filters.php
+++ app/Filters/Filters.php
@@ -61,6 +61,8 @@ abstract class Filters
      */
     public function getFilters()
     {
-        return $this->request->intersect($this->filters);
+        return array_filter($this->request->only($this->filters));
+        
+        // return $this->request->intersect($this->filters);
     }
 }
